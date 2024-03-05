50fcca1eea380cabf8e5402c2083877f33ef822a
himanshu hada <himanshuhada17@gmail.com>
2024-03-05 11:42:05 +0530

    changes

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex justify-center items-center'>
-      <div className='font-light mt-4'>© 2024 Zostels. All rights reserved. Designed and developed with ❤️ by Himanshu Hada</div>
+      <div className='fixed bottom-0 font-light mb-3'>© 2024 Zostels. All rights reserved. Designed and developed with ❤️ by Himanshu Hada</div>
     </div>
   )
 }
