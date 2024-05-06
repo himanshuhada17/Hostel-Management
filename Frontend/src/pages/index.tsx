2286092bcb0e2609c80a61395e48c2a09749b7c8
himanshu hada <himanshuhada17@gmail.com>
2024-05-06 22:54:48 +0530

    changes

--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -5,8 +5,8 @@ import Dashboard from "@/components/dashboard";
 export default function Home() {
   return (
     <>
-    <Rooms/>
-      {/* <Dashboard /> */}
+    {/* <Rooms/> */}
+      <Dashboard />
       {/* <LogIn /> */}
     </>
   );
